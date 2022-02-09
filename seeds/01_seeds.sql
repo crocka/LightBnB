ac4523dd4a7db0d8572910ab478c8275c9d02d9d
crocka <chino123abc@hotmail.com>
2022-02-09 20:30:21 +0000

    create seeds dir and insert data to tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
