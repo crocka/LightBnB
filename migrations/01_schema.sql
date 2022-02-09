e41de95b02aeee82f7773624ebf4a45fa8097708
crocka <chino123abc@hotmail.com>
2022-02-09 20:22:45 +0000

    create users, reservations, property_review tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(225) NOT NULL,
+  email VARCHAR(225) NOT NULL,
+  password VARCHAR(225) NOT NULL
+);
+
+CREATE TABLE properites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(225) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(225) NOT NULL,
+  cover_photo_url VARCHAR(225) NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0, 
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  country VARCHAR(225) NOT NULL,
+  street VARCHAR(225) NOT NULL,
+  city VARCHAR(225) NOT NULL,
+  province VARCHAR(225) NOT NULL,
+  post_code VARCHAR(225) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properites(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properites(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
